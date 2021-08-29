a85470bcc83fe516212b50ca017078e67c826198
Erfan <ucabzaf@ucl.ac.uk>
2021-08-29 20:36:45 +0100

    removed unecessary code

--- WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService.iOS/Services/IOSAudioSessionService.cs
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService.iOS/Services/IOSAudioSessionService.cs
@@ -33,27 +33,5 @@ namespace CognitiveSpeechService.iOS.Services
                 Console.WriteLine(ex.Message);
             }
         }
-
-        // 
-        // one function which just sets category to AVAudioSessionCategoryPlayAndRecord which both enables STT and TTS
-        // although I don't like this option because it would mean PAI is still trying to recognise speech whilst playing out
-        // audio announcement. I don't want this. Whilst user is requesting one bit of info, they shouldnt be allowed to ask for another.
-        // I don't want to handle queuing data queries, etc. 
-        //public void AllowRecordingAndPlayback()
-        //{
-        //    try
-        //    {
-        //        new System.Threading.Thread(new System.Threading.ThreadStart(() =>
-        //        {
-        //            var session = AVAudioSession.SharedInstance();
-        //            session.SetCategory(AVAudioSessionCategory.PlayAndRecord);
-        //            session.SetActive(true);
-        //        })).Start();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //}
     }
 }
